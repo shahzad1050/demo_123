3ef4e9f71f8f59ad0f80b918986189cc8af7fc22
Shahzad1050 <shahzadarham266@gmail.com>
2025-01-06 23:26:26 +0500

    learning

--- goo.py
+++ goo.py
@@ -14,5 +14,14 @@
 # for  i in range (1,11):
 #  print(f" 2 * {i} = {2 * i}")
 
-for  a in range (1,11):
-    print("2 *", a,"=",2*a)
+# for  a in range (1,11):
+#     print("2 *", a,"=",2*a)
+
+# for i in range (1,20):
+#     print(i)
+
+# for i in range (1,20,3):
+#     print(i)
+
+for i in range (1,20,2):
+    print(i)
