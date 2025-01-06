ceb996f304bd76d351b5c84b9c4456eadf4fddf1
Shahzad1050 <shahzadarham266@gmail.com>
2025-01-06 19:41:22 +0500

    learning

--- goo.py
+++ goo.py
@@ -1,2 +1,13 @@
-print("hello shahzad")
-print ("hello shoiab")
+# print("hello shahzad")
+# print ("hello shoaib")
+# name = "mubashir"
+# if name == "shahzad":
+#   print("welcome shahzad")
+# elif name == "asad":
+#      print("asad")
+# elif name =="mubashir":    
+#     print("mubashir")
+# else :
+#   print("aap kon")
+names: "str" = ["shahzad","shoaib","ali"]
+print(names[0],names[2])
