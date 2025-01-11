5662a7493d51cc94603514b4aeee76447e94c04e
Shahzad1050 <shahzadarham266@gmail.com>
2025-01-11 21:38:50 +0500

    learning

--- goo.py
+++ goo.py
@@ -22,20 +22,24 @@
 #     print(i)
 
 # for i in range (1,20,2):
-#     print(i)
+#     print(i)""
 
-# name = input ("what is your name ")
-# print (name)
-
-
-its_raining = "raining"
-its_sunnyday = "sunnyday"
-
-user = input( "write your comment")
-
-if user == its_raining:
-  print("yes its raining " )
-elif user == its_sunnyday:
-   print("yes its a sunnyday")
-else:
-    print(" u have a wrong comment" )
+name: str = ["shahzad","saeed","shoiab","ali","sohaib","faisal","Aamir", "Aisha", "Akbar", "Alina", "Anwar", "Asad", "Asma", "Bilal", "Bushra", "Danish",
+    "Ehsan", "Faiza", "Farhan", "Fatima", "Ghazala", "Habib", "Hina", "Imran", "Jameela", "Kamran",
+    "Khalid", "Lubna", "Mahira", "Mansoor", "Maria", "Mehmood", "Nadia", "Naeem", "Nasir", "Nida",
+    "Noman", "Parveen", "Qasim", "Rabia", "Rahim", "Raza", "Rizwan", "Saima", "Salman", "Sana",
+    "Shahid", "Shazia", "Sohail", "Sumaira", "Tahir", "Tanveer", "Tariq", "Uzma", "Waqar", "Yasir",
+    "Adeel", "Afshan", "Ahmad", "Aiman", "Aisha", "Ali", "Amna", "Anam", "Arif", "Arisha",
+    "Asif", "Atif", "Azhar", "Babar", "Bisma", "Dua", "Emaan", "Fahad", "Faisal", "Farah",
+    "Fariha", "Fawad", "Fiza", "Gul", "Hafsa", "Haleema", "Hamza", "Haris", "Hassan", "Huma",
+    "Ibrahim", "Irfan", "Javeria", "Junaid", "Kashif", "Khadija", "Kiran", "Komal", "Laiba", "Laraib",
+    "Lubna", "Maham", "Mahnoor", "Maira", "Mansoor", "Maryam", "Mehreen", "Moin", "Mubashir", "Mumtaz",
+    "Nabeel", "Nadia", "Naila", "Naveed", "Nazia", "Nimra", "Noman", "Nosheen", "Omar", "Osama",
+    "Parveen", "Qasim", "Rabia", "Rafay", "Rameez", "Rania", "Rashid", "Rehan", "Rida", "Rizwan",
+    "Saad", "Sadia", "Sahar", "Sajid", "Sameer", "Sana", "Sara", "Shahbaz", "Shahzaib", "Shamim",
+    "Shayan", "Sobia", "Sohail", "Sonia", "Sufyan", "Sumaira", "Tabassum", "Tahir", "Talha", "Tanveer",
+    "Tariq", "Tayyab", "Ubaid", "Umar", "Usman", "Uzair", "Waqas", "Yasmin", "Yasir", "Zain",
+    "Zara", "Zeeshan", "Zoya", "Zubair", "Zunaira", "Zeeshan", "Zainab", "Zahid", "Zara", "Zubair"
+]
+for list in name:
+ print(list)
