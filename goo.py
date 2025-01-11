fdccf52860f1d681d65352f4b66d7db53b71a62b
Shahzad1050 <shahzadarham266@gmail.com>
2025-01-11 19:28:11 +0500

    learning

--- goo.py
+++ goo.py
@@ -13,10 +13,10 @@
 # print(names[0],names[2])
 
 # for  a in range (1,11):
-#     print("2 *", a,"=",2*a)
+#   print("2 *", a,"=",2*a)
 
-# for i in range (1,20):
-#     print(i)
+# for i in range (1,20): 
+#   print(i)
 
 # for i in range (1,20,3):
 #     print(i)
@@ -24,5 +24,18 @@
 # for i in range (1,20,2):
 #     print(i)
 
-name = input ("what is your name ")
-print (name)
+# name = input ("what is your name ")
+# print (name)
+
+
+its_raining = "raining"
+its_sunnyday = "sunnyday"
+
+user = input( "write your comment")
+
+if user == its_raining:
+  print("yes its raining " )
+elif user == its_sunnyday:
+   print("yes its a sunnyday")
+else:
+    print(" u have a wrong comment" )
