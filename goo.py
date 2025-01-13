fd6ff7bf7a142632ef48e8169a680a9476463620
Shahzad1050 <shahzadarham266@gmail.com>
2025-01-13 14:07:57 +0500

    learning

--- goo.py
+++ goo.py
@@ -24,25 +24,39 @@
 # for i in range (1,20,2):
 #     print(i)""
 
-name: str = ["shahzad","saeed","shoiab","ali","sohaib","faisal","Aamir", "Aisha", "Akbar", "Alina", "Anwar", "Asad", "Asma", "Bilal", "Bushra", "Danish",
-    "Ehsan", "Faiza", "Farhan", "Fatima", "Ghazala", "Habib", "Hina", "Imran", "Jameela", "Kamran",
-    "Khalid", "Lubna", "Mahira", "Mansoor", "Maria", "Mehmood", "Nadia", "Naeem", "Nasir", "Nida",
-    "Noman", "Parveen", "Qasim", "Rabia", "Rahim", "Raza", "Rizwan", "Saima", "Salman", "Sana",
-    "Shahid", "Shazia", "Sohail", "Sumaira", "Tahir", "Tanveer", "Tariq", "Uzma", "Waqar", "Yasir",
-    "Adeel", "Afshan", "Ahmad", "Aiman", "Aisha", "Ali", "Amna", "Anam", "Arif", "Arisha",
-    "Asif", "Atif", "Azhar", "Babar", "Bisma", "Dua", "Emaan", "Fahad", "Faisal", "Farah",
-    "Fariha", "Fawad", "Fiza", "Gul", "Hafsa", "Haleema", "Hamza", "Haris", "Hassan", "Huma",
-    "Ibrahim", "Irfan", "Javeria", "Junaid", "Kashif", "Khadija", "Kiran", "Komal", "Laiba", "Laraib",
-    "Lubna", "Maham", "Mahnoor", "Maira", "Mansoor", "Maryam", "Mehreen", "Moin", "Mubashir", "Mumtaz",
-    "Nabeel", "Nadia", "Naila", "Naveed", "Nazia", "Nimra", "Noman", "Nosheen", "Omar", "Osama",
-    "Parveen", "Qasim", "Rabia", "Rafay", "Rameez", "Rania", "Rashid", "Rehan", "Rida", "Rizwan",
-    "Saad", "Sadia", "Sahar", "Sajid", "Sameer", "Sana", "Sara", "Shahbaz", "Shahzaib", "Shamim",
-    "Shayan", "Sobia", "Sohail", "Sonia", "Sufyan", "Sumaira", "Tabassum", "Tahir", "Talha", "Tanveer",
-    "Tariq", "Tayyab", "Ubaid", "Umar", "Usman", "Uzair", "Waqas", "Yasmin", "Yasir", "Zain",
-    "Zara", "Zeeshan", "Zoya", "Zubair", "Zunaira", "Zeeshan", "Zainab", "Zahid", "Zara", "Zubair"
-]
-for list in name:
- print(list)
+# name: str = ["shahzad","saeed","shoiab","ali","sohaib","faisal","Aamir", "Aisha", "Akbar", "Alina", "Anwar", "Asad", "Asma", "Bilal", "Bushra", "Danish",
+#     "Ehsan", "Faiza", "Farhan", "Fatima", "Ghazala", "Habib", "Hina", "Imran", "Jameela", "Kamran",
+#     "Khalid", "Lubna", "Mahira", "Mansoor", "Maria", "Mehmood", "Nadia", "Naeem", "Nasir", "Nida",
+#     "Noman", "Parveen", "Qasim", "Rabia", "Rahim", "Raza", "Rizwan", "Saima", "Salman", "Sana",
+#     "Shahid", "Shazia", "Sohail", "Sumaira", "Tahir", "Tanveer", "Tariq", "Uzma", "Waqar", "Yasir",
+#     "Adeel", "Afshan", "Ahmad", "Aiman", "Aisha", "Ali", "Amna", "Anam", "Arif", "Arisha",
+#     "Asif", "Atif", "Azhar", "Babar", "Bisma", "Dua", "Emaan", "Fahad", "Faisal", "Farah",
+#     "Fariha", "Fawad", "Fiza", "Gul", "Hafsa", "Haleema", "Hamza", "Haris", "Hassan", "Huma",
+#     "Ibrahim", "Irfan", "Javeria", "Junaid", "Kashif", "Khadija", "Kiran", "Komal", "Laiba", "Laraib",
+#     "Lubna", "Maham", "Mahnoor", "Maira", "Mansoor", "Maryam", "Mehreen", "Moin", "Mubashir", "Mumtaz",
+#     "Nabeel", "Nadia", "Naila", "Naveed", "Nazia", "Nimra", "Noman", "Nosheen", "Omar", "Osama",
+#     "Parveen", "Qasim", "Rabia", "Rafay", "Rameez", "Rania", "Rashid", "Rehan", "Rida", "Rizwan",
+#     "Saad", "Sadia", "Sahar", "Sajid", "Sameer", "Sana", "Sara", "Shahbaz", "Shahzaib", "Shamim",
+#     "Shayan", "Sobia", "Sohail", "Sonia", "Sufyan", "Sumaira", "Tabassum", "Tahir", "Talha", "Tanveer",
+#     "Tariq", "Tayyab", "Ubaid", "Umar", "Usman", "Uzair", "Waqas", "Yasmin", "Yasir", "Zain",
+#     "Zara", "Zeeshan", "Zoya", "Zubair", "Zunaira", "Zeeshan", "Zainab", "Zahid", "Zara", "Zubair"
+# ]
+# for list in name:
 
-for i in range (1,20,3):
-     print(i)
+#     print(list)
+
+# list1  = ["ali","shahzad","sajjad","faisal"]
+# for h in list1:
+#   list1[3] = "usman"
+#   list1[1] = "junaid"
+#   print(h[1])
+
+# names  = ["ali","shahzad","sajjad","faisal"]
+# for h in names:
+#   names[3] = "usman"
+#   names[1] = "junaid"
+#   print(h)
+
+# tuple =("shahzad","faisal","zahid","junaid")
+# for z in tuple:
+#      print(z)
