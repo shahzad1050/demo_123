dc812a18f58cbb665015afebda40151322d6e63f
Shahzad1050 <shahzadarham266@gmail.com>
2025-01-13 18:12:29 +0500

    assignments

--- goo.py
+++ goo.py
@@ -1,86 +1,90 @@
-print("hello shahzad")
-print ("hello shoaib")
-name = "mubashir"
-if name == "shahzad":
-  print("welcome shahzad")
-elif name == "asad":
-     print("asad")
-elif name =="mubashir":    
-    print("mubashir")
-else :
-  print("aap kon")
-names: "str" = ["shahzad","shoaib","ali"]
-print(names[0],names[2])
+# print("hello shahzad")
+# print ("hello shoaib")
+# name = "mubashir"
+# if name == "shahzad":
+#   print("welcome shahzad")
+# elif name == "asad":
+#      print("asad")
+# elif name =="mubashir":    
+#     print("mubashir")
+# else :
+#   print("aap kon")
+# names: "str" = ["shahzad","shoaib","ali"]
+# print(names[0],names[2])
 
-for  a in range (1,11):
-  print("2 *", a,"=",2*a)
+# for  a in range (1,11):
+#   print("2 *", a,"=",2*a)
 
-for i in range (1,20): 
-  print(i)
+# for i in range (1,20): 
+#   print(i)
 
-for i in range (1,20,3):
-    print(i)
+# for i in range (1,20,3):
+#     print(i)
 
-for i in range (1,20,2):
-    print(i)""
+# for i in range (1,20,2):
+#     print(i)""
 
-name: str = ["shahzad","saeed","shoiab","ali","sohaib","faisal","Aamir", "Aisha", "Akbar", "Alina", "Anwar", "Asad", "Asma", "Bilal", "Bushra", "Danish",
-    "Ehsan", "Faiza", "Farhan", "Fatima", "Ghazala", "Habib", "Hina", "Imran", "Jameela", "Kamran",
-    "Khalid", "Lubna", "Mahira", "Mansoor", "Maria", "Mehmood", "Nadia", "Naeem", "Nasir", "Nida",
-    "Noman", "Parveen", "Qasim", "Rabia", "Rahim", "Raza", "Rizwan", "Saima", "Salman", "Sana",
-    "Shahid", "Shazia", "Sohail", "Sumaira", "Tahir", "Tanveer", "Tariq", "Uzma", "Waqar", "Yasir",
-    "Adeel", "Afshan", "Ahmad", "Aiman", "Aisha", "Ali", "Amna", "Anam", "Arif", "Arisha",
-    "Asif", "Atif", "Azhar", "Babar", "Bisma", "Dua", "Emaan", "Fahad", "Faisal", "Farah",
-    "Fariha", "Fawad", "Fiza", "Gul", "Hafsa", "Haleema", "Hamza", "Haris", "Hassan", "Huma",
-    "Ibrahim", "Irfan", "Javeria", "Junaid", "Kashif", "Khadija", "Kiran", "Komal", "Laiba", "Laraib",
-    "Lubna", "Maham", "Mahnoor", "Maira", "Mansoor", "Maryam", "Mehreen", "Moin", "Mubashir", "Mumtaz",
-    "Nabeel", "Nadia", "Naila", "Naveed", "Nazia", "Nimra", "Noman", "Nosheen", "Omar", "Osama",
-    "Parveen", "Qasim", "Rabia", "Rafay", "Rameez", "Rania", "Rashid", "Rehan", "Rida", "Rizwan",
-    "Saad", "Sadia", "Sahar", "Sajid", "Sameer", "Sana", "Sara", "Shahbaz", "Shahzaib", "Shamim",
-    "Shayan", "Sobia", "Sohail", "Sonia", "Sufyan", "Sumaira", "Tabassum", "Tahir", "Talha", "Tanveer",
-    "Tariq", "Tayyab", "Ubaid", "Umar", "Usman", "Uzair", "Waqas", "Yasmin", "Yasir", "Zain",
-    "Zara", "Zeeshan", "Zoya", "Zubair", "Zunaira", "Zeeshan", "Zainab", "Zahid", "Zara", "Zubair"
-]
-for list in name:
+# name: str = ["shahzad","saeed","shoiab","ali","sohaib","faisal","Aamir", "Aisha", "Akbar", "Alina", "Anwar", "Asad", "Asma", "Bilal", "Bushra", "Danish",
+#     "Ehsan", "Faiza", "Farhan", "Fatima", "Ghazala", "Habib", "Hina", "Imran", "Jameela", "Kamran",
+#     "Khalid", "Lubna", "Mahira", "Mansoor", "Maria", "Mehmood", "Nadia", "Naeem", "Nasir", "Nida",
+#     "Noman", "Parveen", "Qasim", "Rabia", "Rahim", "Raza", "Rizwan", "Saima", "Salman", "Sana",
+#     "Shahid", "Shazia", "Sohail", "Sumaira", "Tahir", "Tanveer", "Tariq", "Uzma", "Waqar", "Yasir",
+#     "Adeel", "Afshan", "Ahmad", "Aiman", "Aisha", "Ali", "Amna", "Anam", "Arif", "Arisha",
+#     "Asif", "Atif", "Azhar", "Babar", "Bisma", "Dua", "Emaan", "Fahad", "Faisal", "Farah",
+#     "Fariha", "Fawad", "Fiza", "Gul", "Hafsa", "Haleema", "Hamza", "Haris", "Hassan", "Huma",
+#     "Ibrahim", "Irfan", "Javeria", "Junaid", "Kashif", "Khadija", "Kiran", "Komal", "Laiba", "Laraib",
+#     "Lubna", "Maham", "Mahnoor", "Maira", "Mansoor", "Maryam", "Mehreen", "Moin", "Mubashir", "Mumtaz",
+#     "Nabeel", "Nadia", "Naila", "Naveed", "Nazia", "Nimra", "Noman", "Nosheen", "Omar", "Osama",
+#     "Parveen", "Qasim", "Rabia", "Rafay", "Rameez", "Rania", "Rashid", "Rehan", "Rida", "Rizwan",
+#     "Saad", "Sadia", "Sahar", "Sajid", "Sameer", "Sana", "Sara", "Shahbaz", "Shahzaib", "Shamim",
+#     "Shayan", "Sobia", "Sohail", "Sonia", "Sufyan", "Sumaira", "Tabassum", "Tahir", "Talha", "Tanveer",
+#     "Tariq", "Tayyab", "Ubaid", "Umar", "Usman", "Uzair", "Waqas", "Yasmin", "Yasir", "Zain",
+#     "Zara", "Zeeshan", "Zoya", "Zubair", "Zunaira", "Zeeshan", "Zainab", "Zahid", "Zara", "Zubair"
+# ]
+# for list in name:
 
-    print(list)
+#     print(list)
 
-list1  = ["ali","shahzad","sajjad","faisal"]
-for h in list1:
-  list1[3] = "usman"
-  list1[1] = "junaid"
-  print(h[1])
+# list1  = ["ali","shahzad","sajjad","faisal"]
+# for h in list1:
+#   list1[3] = "usman"
+#   list1[1] = "junaid"
+#   print(h[1])
 
-names  = ["ali","shahzad","sajjad","faisal"]
-for h in names:
-  names[3] = "usman"
-  names[1] = "junaid"
-  print(h)
+# names  = ["ali","shahzad","sajjad","faisal"]
+# for h in names:
+#   names[3] = "usman"
+#   names[1] = "junaid"
+#   print(h)
 
-tuple =("shahzad","faisal","zahid","junaid")
-for z in tuple:
-     print(z)
+# tuple =("shahzad","faisal","zahid","junaid")
+# for z in tuple:
+#      print(z)
 
-number : int = 1
-while number  <= 12:
-  number = number + 1
-  print(number)
+# number : int = 1
+# while number  <= 12:
+#   number = number + 1
+#   print(number)
 
   
-number : int = 1
-while number  <= 12:
-  print(number)
-  number = number + 1
+# number : int = 1
+# while number  <= 12:
+#   print(number)
+#   number = number + 1
   
-user_name = input("enter your name")
-user_marks = int (input("enter your marks"))
-if user_marks >= 90:
-  print("GARDE A")
-elif user_marks >= 80:
-   print("GARDE B")
-elif user_marks >= 70:
-  print("GARDE C")
-elif user_marks >= 60:
-   print(" GARDE D")
-else: 
-   print("you are fail")
+# user_name = input("enter your name")
+# user_marks = int (input("enter your marks"))
+# if user_marks >= 90:
+#   print("GARDE A")
+# elif user_marks >= 80:
+#    print("GARDE B")
+# elif user_marks >= 70:
+#   print("GARDE C")
+# elif user_marks >= 60:
+#    print(" GARDE D")
+# else: 
+#    print("you are fail")
+
+user_input =int (input("enter the table u want to print"))
+for i in range(1,11):
+   print(f"{user_input} x {i} = {user_input * i}") 
