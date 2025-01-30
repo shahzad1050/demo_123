f91ff95cbd66fc89e1b61523e8f96592e0d4d47b
Shahzad1050 <shahzadarham266@gmail.com>
2025-01-30 19:38:37 +0500

    learning

--- name.py
+++ name.py
@@ -4,18 +4,18 @@ def play():
     user = input("what is choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r','p','s'])
 
-    if user == computer:
+    if user == (computer):
         return 'it\'s a tie'
 
-        if is_win(user, computer):
-            return 'you won!'
+    if is_win(user, computer):
+        return 'you won!'
+    return' you lost!'
 
-            return' you lost!'
+def is_win (player, oppnent):
+    if player == ('r' and oppnent == 's') or (player == 's' and oppnent =='p') or player == ('p' and oppnent == 'r'):
+        return True
+    return False  
+  
+result = play()
+print(result)
 
-def is_win (player, oppnent):    
-
-        if player == ('r' and oppnent == 's') or (player == 's' and oppnent =='p') \
-            or player == ('p' and oppnent == 'r'):
-
-            return True
-            
